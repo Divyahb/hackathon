c7a561e954264d8a4e401cb7d19c63573f639566
x009016 <x009016@Divyas-MacBook-Pro.local>
2018-03-02 11:14:40 +0100

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.post('/identity', function(req, res, next) {
+  var validUser = true;
+  var userResponse = {
+    'name': 'Jan',
+    'flyingBlueNumber': '232323'
+  }
+  var userDetails = {
+    'name': userResponse.name,
+    'flyingBlueNumber': userResponse.flyingBlueNumber
+  }
+  var errorStatus = {
+    'error': 'No matching user found'
+  }
+  if(validUser) {
+    res.send(userDetails);
+  }else {
+    res.send(error);
+  }
+});
+
+router.post('/register', function(req, res, next) {
+  var userDetails = {
+    'name': req.body.name,
+    'flyingBlueNumber': req.body.flyingBlueNumber
+  }
+  res.send('OK');
+});
+module.exports = router;
+
