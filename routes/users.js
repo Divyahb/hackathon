9bfa756c85209a61d96dd6fc7a11af66b476c7ab
x009016 <x009016@Divyas-MacBook-Pro.local>
2018-03-02 13:11:43 +0100

    Done get method

--- routes/users.js
+++ routes/users.js
@@ -22,6 +22,27 @@ router.post('/identity', function(req, res, next) {
   }
 });
 
+/* GET users listing. */
+router.get('/identity', function(req, res, next) {
+  var validUser = true;
+  var userResponse = {
+    'name': 'Jan',
+    'flyingBlueNumber': '232323'
+  }
+  var userDetails = {
+    'name': userResponse.name,
+    'flyingBlueNumber': userResponse.flyingBlueNumber
+  }
+  var errorStatus = {
+    'error': 'No matching user found'
+  }
+  if(validUser) {
+    res.send(userDetails);
+  }else {
+    res.send(error);
+  }
+});
+
 router.post('/register', function(req, res, next) {
   var userDetails = {
     'name': req.body.name,
